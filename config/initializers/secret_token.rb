f92a304da3dd53313308f093531cf03c15919686
Leland Husband <leland.husband@gmail.com>
2011-05-23 08:16:59 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pkproject::Application.config.secret_token = 'a2596e05f3cbf2f93235c5254c8ac48e06c1ebc231feed8e1b53173dc6296aa11a3d7c379afe0221651e5a20b0d17ad773f0c009a7674c0822a22f9c8d97bd9a'
