e33f0cdad1b554c6bebbcd4fd327560c76676b55
Leland Husband <leland.husband@gmail.com>
2011-05-23 15:23:37 -0500

    Adding a topNav feature.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Pkproject::Application.routes.draw do
   
-  root :to => 'pages#home'
+  get '/about' => 'pages#about', as: 'about'
+  get '/help' => 'pages#help', as: 'help'
+  get '/contact' => 'pages#contact', as: 'contact'
+  get '/bricks' => 'pages#bricks', as: 'bricks'
+  get '/categories' => 'pages#categories', as: 'categories'
+  get '/state' => 'pages#states', as: 'states'
+  
+  root to: 'pages#home'
 end
